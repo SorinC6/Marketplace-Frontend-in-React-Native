2e9c6a62eb6f9031965e2c9d34dafd53c2059a3f
SorinC6 <kish.sorin@yahoo.com>
2022-11-25 16:19:05 +0200

    add part of the card

--- screens/Home.js
+++ screens/Home.js
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import { Text, SafeAreaView, FlatList, View } from 'react-native'
-import { FocusedStatusBar } from '../components'
+import { FocusedStatusBar, HomeHeader, NftCard } from '../components'
 import { NFTData, COLORS } from '../constants'
 
 
@@ -8,6 +8,21 @@ const Home = () => {
     return (
         <SafeAreaView style={{ flex: 1 }}>
             <FocusedStatusBar backgroundColor={COLORS.primary} />
+            <View style={{ flex: 1, zIndex: 1 }}>
+                <FlatList
+                    data={NFTData}
+                    keyExtractor={(item) => item.id}
+                    renderItem={({ item }) => {
+                        return <NftCard data={item} />
+                    }}
+                    showsVerticalScrollIndicator={false}
+                    ListHeaderComponent={<HomeHeader />}
+                />
+            </View>
+            <View style={{ position: 'absolute', top: 0, bottom: 0, right: 0, left: 0, zIndex: -1 }}>
+                <View style={{ height: 300, backgroundColor: COLORS.primary }} />
+                <View style={{ flex: 1, backgroundColor: COLORS.white }} />
+            </View>
         </SafeAreaView>
     )
 }
